41cd014aa31266de29b08b49c628f3ea17f13d96
Tiago Lopo Da Silva <tlopo@ibm.com>
2018-01-20 20:26:27 +0100

    Added test loading config from object

--- lib/tlopo/cli/version.rb
+++ lib/tlopo/cli/version.rb
@@ -1,5 +1,5 @@
 module Tlopo
-  module Cli
+  class Cli
     VERSION = '0.1.0'.freeze
   end
 end
--- test/tlopo/cli_test.rb
+++ test/tlopo/cli_test.rb
@@ -1,13 +1,55 @@
 require 'test_helper'
 
+# It must load config from file
+# It must load config from object
+# It must bomb validating all fields of command
+# It must bomb validating all fields of switches
+# It must create _globals
+# It must create at least 5 subcommands
+
 module Tlopo
   class CliTest < Minitest::Test
+    @@command_cfg = {
+        'name' => 'command',
+        'class' => 'Tlopo::Command',
+        'banner' => 'Banner goes here',
+        'switches' => [
+          {
+            'name' => 'filename',
+            'short' => '-F',
+            'long' => '--filename <filename>',
+            'desc' => 'Sets filename'
+          }
+        ]
+    }
+
     def test_that_it_has_a_version_number
       refute_nil(::Tlopo::Cli::VERSION)
     end
 
-    def test_it_does_something_useful
+    def test_that_it_loads_config_from_file
       assert false
+    end
+
+    def test_that_it_loads_config_from_object
+      filename = '/etc/hosts'
+      Tlopo::Command.opt 'filename'
+      out, err = capture_io do 
+        ARGV.replace ['-F', filename ]
+        Tlopo::Cli.new( config: @@command_cfg ).run
+      end
+      assert_equal(filename, out.strip)
+    end
+  end
+
+  class Command
+    def self.opt (value=nil)
+      @@value = value unless value.nil?
+      return @@value
+    end 
+
+    def self.run(opts)
+      puts opts[@@value]
     end
   end
 end
